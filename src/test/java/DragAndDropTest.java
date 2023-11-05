43cbdd51b6f94bcf961086a775c1eaee1405b8f6
AnnTs <atserkovnikova.qa@gmail.com>
2023-11-05 18:13:25 +0300

    added homework-2 DragAndDropTest

---
+++ src/test/java/DragAndDropTest.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+    }
+
+
+
+    @Test
+    void exchangeFiguresByDragAndDrop() {
+        open("/drag_and_drop");
+        $(withText("Drag and Drop")).shouldBe(visible);
+        $("#column-a").$("header").shouldHave(text("A"));
+        $("#column-b").$("header").shouldHave(text("B"));
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A"));
+
+    }
+    @AfterAll
+    static void afterAll() {
+        Selenide.closeWebDriver();
+    }
+}
