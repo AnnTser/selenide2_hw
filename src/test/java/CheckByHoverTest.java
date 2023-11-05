7239ba0497894cf7f53079b752049235df1edda4
AnnTs <atserkovnikova.qa@gmail.com>
2023-11-05 17:13:13 +0300

    added homework

---
+++ src/test/java/CheckByHoverTest.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CheckByHoverTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void selectEnterprizeByHover() {
+
+        open("/");
+        $(".header-menu-wrapper").$(byTagAndText("button", "Solutions")).hover();
+        $(".header-menu-wrapper").$(byTagAndText("a", "Enterprise")).click();
+        $(withText("To build, scale, and deliver secure software.")).shouldBe(visible);
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+        Selenide.closeWebDriver();
+    }
+}
